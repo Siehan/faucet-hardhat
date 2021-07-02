fcb3e62816748af0ab8cb0fc13efd547235d39b3
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-07-03 00:32:24 +0200

    Update RobinetToken-test.js

--- test/RobinetToken-test.js
+++ test/RobinetToken-test.js
@@ -1,33 +1,36 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('robinettoken', function () {
-  let RobinetToken, robinettoken, dev, owner;
-  let TOTAL_SUPPLY = ethers.utils.parseEther('1000000');
-  let ZERO_ADDRESS = ethers.constants.AddressZero;
+describe('robinetToken', function () {
+  let RobinetToken, robinetToken, owner;
+  const INITIAL_SUPPLY = '1000000';
+  const TOTAL_SUPPLY = ethers.utils.parseEther('1000000');
+  const ZERO_ADDRESS = ethers.constants.AddressZero;
   const NAME = 'RobinetToken';
   const SYMBOL = 'RBT';
 
   beforeEach(async function () {
-    ;[dev, owner] = await ethers.getSigners();
+    [owner] = await ethers.getSigners();
     RobinetToken = await ethers.getContractFactory('RobinetToken');
-    robinettoken = await RobinetToken.connect(owner).deploy(TOTAL_SUPPLY);
-    await robinettoken.deployed();
-  })
+    robinetToken = await RobinetToken.connect(owner).deploy(INITIAL_SUPPLY);
+    await robinetToken.deployed();
+  });
+
   it(`Should have name: ${NAME}`, async function () {
-    expect(await robinettoken.name()).to.equal(NAME);
+    expect(await robinetToken.name()).to.equal(NAME);
   });
 
   it(`Should have symbol: ${SYMBOL}`, async function () {
-    expect(await robinettoken.symbol()).to.equal(SYMBOL);
+    expect(await robinetToken.symbol()).to.equal(SYMBOL);
   });
 
-  it('should emit a Transfer event', async function () {
-    expect(robinettoken.deployTransaction).to.emit(robinettoken, 'Transfer')
-    .withArgs(ZERO_ADDRESS, owner.address, TOTAL_SUPPLY);
+  it('should emit a Transer event', async function () {
+    expect(robinetToken.deployTransaction)
+      .to.emit(robinetToken, 'Transfer')
+      .withArgs(ZERO_ADDRESS, owner.address, TOTAL_SUPPLY);
   });
 
   it('should transfer the total supply to owner', async function () {
-    expect(await robinettoken.balanceOf(owner.address)).to.equal(TOTAL_SUPPLY);
+    expect(await robinetToken.balanceOf(owner.address)).to.equal(TOTAL_SUPPLY);
   });
 });
