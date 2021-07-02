1e842b0a1ffc1bfdfbf283a38c427b3f02624c7b
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-07-03 00:29:25 +0200

    Update Faucet-test.js

--- test/Faucet-test.js
+++ test/Faucet-test.js
@@ -2,30 +2,35 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Faucet', function () {
-  let Faucet, faucet, RobinetToken, robinettoken, dev, owner, alice;
-  const TOTAL_SUPPLY = ethers.utils.parseEther('10000000');
+  let Faucet, faucet, RobinetToken, robinetToken, owner, alice;
+  const TOTAL_SUPPLY = ethers.utils.parseEther('1000000');
+  const TOKEN_AMOUNT = ethers.utils.parseEther('100');
   const TWO_DAYS = 172800;
   const THREE_DAYS = 259200;
 
   beforeEach(async function () {
-    [dev, owner, alice] = await ethers.getSigners();
+    [owner, alice] = await ethers.getSigners();
     RobinetToken = await ethers.getContractFactory('RobinetToken');
-    robinettoken = await RobinetToken.connect(owner).deploy(TOTAL_SUPPLY);
-    await robinettoken.deployed();
+    robinetToken = await RobinetToken.connect(owner).deploy(1000000);
+    await robinetToken.deployed();
 
     Faucet = await ethers.getContractFactory('Faucet');
-    faucet = await Faucet.connect(owner).deploy(robinettoken.address);
+    faucet = await Faucet.connect(owner).deploy(robinetToken.address);
     await faucet.deployed();
-    await robinettoken.connect(owner).approve(faucet.address, TOTAL_SUPPLY);
+    await robinetToken.connect(owner).approve(faucet.address, TOTAL_SUPPLY);
   });
 
   describe('Deployment', function () {
     it('should set the token contract address', async function () {
-      expect(await faucet.tokenContractAddress()).to.equal(robinettoken.address);
+      expect(await faucet.tokenContractAddress()).to.equal(robinetToken.address);
     });
 
     it('should set the token owner address', async function () {
       expect(await faucet.tokenOwner()).to.equal(owner.address);
+    });
+
+    it('should check allowance of faucet', async function () {
+      expect(await robinetToken.allowance(owner.address, faucet.address)).to.equal(TOTAL_SUPPLY);
     });
   });
 
@@ -34,25 +39,22 @@ describe('Faucet', function () {
       await faucet.connect(alice).claim();
     });
 
-    it('should revert if faucet time not reach', async function () {
+    it('should revert if faucet time not reached', async function () {
       await ethers.provider.send('evm_increaseTime', [TWO_DAYS]);
       await ethers.provider.send('evm_mine');
-      await expect(faucet.connect(alice).claim()).to.revertedWith(
-        'Faucet: You need to wait 3 days before reclaim new Tokens');
+      await expect(faucet.connect(alice).claim())
+        .to.revertedWith('Faucet: You need to wait 3 days before reclaim new Tokens');
     });
 
     it('should get token after 3 days', async function () {
       await ethers.provider.send('evm_increaseTime', [THREE_DAYS]);
       await ethers.provider.send('evm_mine');
       expect(await faucet.connect(alice).claim());
-    });
-
-    it('should receive tokens', async function () {
-      expect(await robinettoken.balanceOf(alice.address)).to.equal(100);
+      expect(await robinetToken.balanceOf(alice.address)).to.equal(TOKEN_AMOUNT.mul(2));
     });
 
     it('should decrease supply for the faucet', async function () {
-      expect(await faucet.supplyInStock()).to.equal(TOTAL_SUPPLY.sub(100));
+      expect(await faucet.supplyInStock()).to.equal(TOTAL_SUPPLY.sub(TOKEN_AMOUNT));
     });
   });
 });
