619166fec483623ee19304a7890be75f6ce597ce
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-07-03 00:15:29 +0200

    Update RobinetToken.sol

--- contracts/RobinetToken.sol
+++ contracts/RobinetToken.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.5;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 /// @title RobinetToken
-/// @author Red Team : Johnatan, Victor, Nassim et Sylvie
+/// @author Red Team : Jonathan, Victor, Nassim et Sylvie
 
 contract RobinetToken is ERC20, Ownable {
     constructor(uint256 totalSupply_) ERC20("RobinetToken", "RBT") {
-        _mint(msg.sender, totalSupply_ * 10 ** decimals());
+        _mint(msg.sender, totalSupply_ * 10**decimals());
     }
 }
